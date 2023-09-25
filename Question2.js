fea125781baf98978692466754db6b48b6f8c1a6
Ivan Fedotovsky <fedotovsky96@gmail.com>
2023-09-25 19:07:35 +0400

    update comments in Question2

--- Question2.js
+++ Question2.js
@@ -4,18 +4,18 @@ function doSmth() {
 
 doSmth()
   .then(function (a) {
-    console.log("1", a); //"123"
-    return a;
+    console.log("1", a); //"1 123"
+    return a; 
   })
   .then(function (b) {
-    console.log("2", b); //"123"
+    console.log("2", b); //"2 123"
     return Promise.reject("321");
   })
   .catch(function (err) {
-    console.log("3", err); //"321"
+    console.log("3", err); //"3 321"
   })
   .then(function (c) {
-    console.log("4", c); //undefined
+    console.log("4", c); //"4" undefined
     return c;
   });
 
